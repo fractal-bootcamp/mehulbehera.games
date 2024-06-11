5e451b3b6ddd41978eb9da25f99e270b14bf331f
Mehul Behera <mbehera@umd.edu>
2024-06-11 16:07:32 -0400

    added message indicating if user is signed in

--- Authentication/Clerk/Clerk/src/App.tsx
+++ Authentication/Clerk/Clerk/src/App.tsx
@@ -9,22 +9,34 @@ import {
 
 function App() {
   return (
-    <div className="navbar bg-base-800 rounded">
-      <div className="flex-1">
-        <a className=" text-xl text-bold">Mehul's Portfolio</a>
-      </div>
-      <div className="flex">
-        <div>
-          <div className="btn btn-ghost  avatar">
-            <SignedOut>
-              <SignInButton />
-            </SignedOut>
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
+    <div>
+      <div className="navbar bg-base-800 rounded">
+        <div className="flex-1">
+          <a className=" text-xl text-bold">Mehul's Portfolio</a>
+        </div>
+        <div className="flex">
+          <div>
+            <div className="btn btn-ghost  avatar">
+              <SignedOut>
+                <SignInButton />
+              </SignedOut>
+              <SignedIn>
+                <UserButton />
+              </SignedIn>
+            </div>
           </div>
         </div>
       </div>
+      <SignedOut>
+        <p className="text-gray-900 text-9xl dark:text-white">
+          You are Signed Out
+        </p>
+      </SignedOut>
+      <SignedIn>
+        <p className="text-gray-900 text-9xl dark:text-white">
+          You Are Signed In
+        </p>
+      </SignedIn>
     </div>
   );
 }
