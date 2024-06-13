5b6d0f70d9c7e0c1d709d7464361cb7e867c8c19
Mehul Behera <mbehera@umd.edu>
2024-06-13 17:16:45 -0400

    added better UI

--- TicTacToe/TicTacToe/src/App.tsx
+++ TicTacToe/TicTacToe/src/App.tsx
@@ -1,12 +1,7 @@
-import { MouseEvent, MouseEventHandler, useState } from "react";
+import { MouseEvent, MouseEventHandler, useEffect, useState } from "react";
 import "./App.css";
 
 const emptyBoard: string[] = ["", "", "", "", "", "", "", "", ""];
-
-interface player {
-  //X or O
-  piece: string;
-}
 
 interface winnerOutput {
   //X, O, null
@@ -67,90 +62,251 @@ function App() {
   const [board, setBoard] = useState(emptyBoard);
   const [currentPlayer, setCurrentPlayer] = useState("X");
   const [winner, setWinner] = useState("");
+  const [singlePlayer, setSinglePlayer] = useState(false);
 
-  function handlePress(e: MouseEvent<HTMLButtonElement>) {
-    //change "ghost board" && checkValid
-    let newBoard = { ...board };
-    const buttonID = parseInt(e.currentTarget.id);
-    if (newBoard[buttonID] === "") {
-      newBoard[buttonID] = currentPlayer;
-      setBoard(newBoard);
-    } else {
-      //user put invalid move
-      return;
+  //numWins: {X Wins: 0, Ties, O Wins}
+  const [numXWins, setXWins] = useState(0);
+  const [numOWins, setOWins] = useState(0);
+  const [numTies, setTies] = useState(0);
+
+  //progress
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    if (currentPlayer === "O" && winner === "" && singlePlayer === false) {
+      const index = getOpponentMove(board, currentPlayer);
+      setPieceOnBoard(index);
     }
+  }, [board]);
 
-    //check winner
-    console.log(newBoard);
-    const output = checkWinState(newBoard);
+  const togglePlayer = () => {
+    setCurrentPlayer(currentPlayer === "X" ? "O" : "X");
+  };
 
-    console.log(output);
+  function setPieceOnBoard(index: number) {
+    let newBoard = [...board];
+
+    if (newBoard[index] === "") {
+      newBoard[index] = currentPlayer;
+      setBoard(newBoard);
+
+      //switch players - for 2 players
+      togglePlayer();
+      setProgress(progress + 1 / 9);
+
+      const output = checkWinState(newBoard);
+
+      const winningOutcome = output.winningOutcome;
+      const winningPiece = output.winningPiece;
+
+      //if won - ADD STOP PLAYERS FROM WINNING
+      if (winningOutcome === "WIN") {
+        setProgress(1);
+        setWinner(winningPiece + "    WINS");
+        if (winningPiece === "X") {
+          setXWins(numXWins + 1);
+        } else {
+          setOWins(numOWins + 1);
+        }
+      } else if (winningOutcome === "TIE") {
+        setProgress(1);
+        setWinner("TIE");
+        setTies(numTies + 1);
+      }
+    }
+  }
+
+  //params: board, depth(how many layers deep), maxmize (boolean whether or not to max), player ("X"|"O")
+  //returns score of a board in a terminal case
+  function minMax(
+    boardToMinMax: typeof board,
+    depth: number,
+    maximize: boolean,
+    player: string
+  ): number {
+    //Base Case: If game is in terminal state evaluate board and return a score
+    const output = checkWinState(boardToMinMax);
 
     const winningOutcome = output.winningOutcome;
     const winningPiece = output.winningPiece;
 
-    //if won - ADD STOP PLAYERS FROM WINNING
-    if (winningOutcome === "WIN") {
-      setWinner(winningPiece + "    WINS");
-    } else if (winningOutcome === "TIE") {
-      setWinner("TIE");
+    if (winningOutcome !== null) {
+      if (winningOutcome === "WIN") {
+        //player wins - return a positive score based on how many layers deep they win
+        if (winningPiece === player) {
+          return 10 - depth;
+        }
+        //opponent wins - return a negative score based on how many layers deep
+        else {
+          return depth - 10;
+        }
+      }
+      //if a tie - return 0
+      if (winningOutcome === "TIE") {
+        return 0;
+      }
     }
 
-    //switch players
-    if (currentPlayer === "X") {
-      setCurrentPlayer("O");
-    } else {
-      setCurrentPlayer("X");
+    //Recursive Function
+
+    //set opponent
+    const opponent = player === "X" ? "O" : "X";
+
+    //maximizing half
+    if (maximize) {
+      let bestScore = -Infinity;
+
+      //for all empty spots on board
+      for (let i = 0; i < boardToMinMax.length; i++) {
+        if (boardToMinMax[i] === "") {
+          //set piece on potential board
+          const newBoard = [...boardToMinMax];
+          newBoard[i] = opponent;
+
+          //get minMaxScore on potential board
+          const score = minMax(newBoard, depth + 1, false, player);
+          bestScore = Math.max(score, bestScore);
+        }
+      }
+      return bestScore;
     }
+    //minimizing half
+    else {
+      let bestScore = Infinity;
+
+      //get all potential boards from empty spots
+      for (let i = 0; i < boardToMinMax.length; i++) {
+        if (boardToMinMax[i] === "") {
+          const newBoard = [...boardToMinMax];
+          newBoard[i] = player;
+
+          const score = minMax(newBoard, depth + 1, true, player);
+          bestScore = Math.min(score, bestScore);
+        }
+      }
+
+      return bestScore;
+    }
+  }
+
+  //params: Takes in current board and current player(opponent)
+  //returns: index of best Move
+  function getOpponentMove(boardToMoveOn: string[], player: string): number {
+    let scores: { score: number; index: number }[] = [];
+    //get the scores of all potential terminal states based on params
+    for (let i = 0; i < boardToMoveOn.length; i++) {
+      if (boardToMoveOn[i] === "") {
+        const potentialBoard = [...boardToMoveOn];
+        potentialBoard[i] = player;
+        const scoreFromMinMax = minMax(potentialBoard, 0, true, player);
+        scores.push({ score: scoreFromMinMax, index: i });
+      }
+    }
+
+    //get the index of the highest score - best move
+    let maxIndex = -1;
+    let maxScore = -Infinity;
+
+    for (let i = 0; i < scores.length; i++) {
+      const scoreToEval = scores[i].score;
+      const indexToEval = scores[i].index;
+
+      if (maxScore < scoreToEval) {
+        maxScore = scoreToEval;
+        maxIndex = indexToEval;
+      }
+    }
+
+    return maxIndex;
+  }
+
+  function handlePress(e: MouseEvent<HTMLButtonElement>) {
+    const buttonID = parseInt(e.currentTarget.id);
+    //add user piece
+    setPieceOnBoard(buttonID);
   }
 
   const hasWon = winner !== "";
 
+  let bgColor = "bg-gray-200";
+
   return (
     <>
-      <div>
-        <div id="row1">
-          <button id="0" disabled={hasWon} onClick={(e) => handlePress(e)}>
-            {board[0]}
+      <section className="grid grid-cols-3 place-items-center">
+        {board.map((tile, index) => (
+          <button
+            className={
+              " btn my-2 hover:bg-gray-300 w-44 h-44 btn-lg text-7xl disabled:bg-orange-300 disabled:text-black text-black " +
+              bgColor
+            }
+            id={index.toString()}
+            disabled={hasWon}
+            onClick={(e) => handlePress(e)}
+          >
+            {board[index]}
           </button>
-          <button id="1" disabled={hasWon} onClick={(e) => handlePress(e)}>
-            {board[1]}
-          </button>
-          <button id="2" disabled={hasWon} onClick={(e) => handlePress(e)}>
-            {board[2]}
-          </button>
-        </div>
-        <div id="row2">
-          <button id="3" disabled={hasWon} onClick={(e) => handlePress(e)}>
-            {board[3]}
-          </button>
-          <button id="4" disabled={hasWon} onClick={(e) => handlePress(e)}>
-            {board[4]}
-          </button>
-          <button id="5" disabled={hasWon} onClick={(e) => handlePress(e)}>
-            {board[5]}
-          </button>
-        </div>
-        <div id="row3">
-          <button id="6" disabled={hasWon} onClick={(e) => handlePress(e)}>
-            {board[6]}
-          </button>
-          <button id="7" disabled={hasWon} onClick={(e) => handlePress(e)}>
-            {board[7]}
-          </button>
-          <button id="8" disabled={hasWon} onClick={(e) => handlePress(e)}>
-            {board[8]}
-          </button>
-        </div>
-      </div>
+        ))}
+      </section>
+
+      <section className="grid grid-cols-3 gap-1 mx-auto mb-10 place-items-center">
+        <button
+          className="btn m-2 bg-gray-200 hover:bg-gray-200 btn-lg w-44 disabled:bg-orange-300 text-black disabled:text-black "
+          id="XWinButton"
+          disabled={hasWon}
+        >
+          X Won: {numXWins}
+        </button>
+        <button
+          className="btn m-2 bg-gray-200 hover:bg-gray-200 btn-lg w-44 disabled:bg-orange-300 text-black disabled:text-black "
+          id="Tie"
+          disabled={hasWon}
+        >
+          Ties: {numTies}
+        </button>
+        <button
+          className="btn m-2 bg-gray-200 hover:bg-gray-200 btn-lg w-44 disabled:bg-orange-300 text-black disabled:text-black "
+          id="OWinButton"
+          disabled={hasWon}
+        >
+          O Won: {numOWins}
+        </button>
+      </section>
+
       <button
+        className="btn m-2 btn-info btn-lg disabled:bg-red-300 w-44 hover:bg-gray-300 bg-gray-200"
         onClick={() => {
-          setBoard(emptyBoard), setWinner("");
+          setBoard(emptyBoard),
+            setWinner(""),
+            setCurrentPlayer("X"),
+            setProgress(0);
         }}
       >
-        Clear
+        {!winner ? "Reset" : "Next Game"}
       </button>
-      <div>Outcome: {winner}</div>
+
+      <div>
+        <div>Outcome: {winner}</div>
+        <progress className="progress w-56" value={progress}></progress>
+      </div>
+
+      <label className="swap swap-flip text-9xl">
+        <input type="checkbox" onClick={() => setSinglePlayer(!singlePlayer)} />
+
+        <div className="swap-on">🙋🏾‍♂️</div>
+        <div className="swap-off">🤖</div>
+      </label>
+
+      {!winner && <div data-modal-show="winner_modal"></div>}
+
+      <dialog id="winner_modal" className="modal">
+        <div className="modal-box">
+          <h3 className="font-bold text-lg">Hello!</h3>
+          <p className="py-4">Press ESC key or click outside to close</p>
+        </div>
+        <form method="dialog" className="modal-backdrop">
+          <button>close</button>
+        </form>
+      </dialog>
     </>
   );
 }
